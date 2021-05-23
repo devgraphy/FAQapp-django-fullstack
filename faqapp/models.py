54dadbfb7a94a38e4995f02d7bd4568218c4b0a8
Changjun Kim <beaican530@gmail.com>
2021-05-23 22:55:07 +0900

    Add Question, Answer data

--- faqapp/models.py
+++ faqapp/models.py
@@ -7,6 +7,9 @@ class Question(models.Model):
     content = models.TextField()                # 글자 수 제한이 없는 데이터
     create_date = models.DateTimeField()      # 날짜, 시간 관련 속성
 
+    def __str__(self):
+        return self.subject
+
 class Answer(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     content = models.TextField()
