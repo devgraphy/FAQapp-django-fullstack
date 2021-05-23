d3f851ea9b53fb6d147351b3135440a4f57e04b9
Changjun Kim <beaican530@gmail.com>
2021-05-23 16:18:43 +0900

    fixes #1 no module DateTimeField

--- faqapp/models.py
+++ faqapp/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)  # 최대 200자까지 입력
     content = models.TextField()                # 글자 수 제한이 없는 데이터
-    create_date = models.DataTimeField()        # 날짜, 시간 관련 속성
+    create_date = models.DateTimeField()      # 날짜, 시간 관련 속성
 
