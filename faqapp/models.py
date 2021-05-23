26cd4a9f52ad1870b963ea9e2fed4f05748fab8d
Changjun Kim <beaican530@gmail.com>
2021-05-23 16:02:46 +0900

    #1 models module has no attribute DataTimeField

--- faqapp/models.py
+++ faqapp/models.py
@@ -1,3 +1,9 @@
+from typing import ContextManager
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)  # 최대 200자까지 입력
+    content = models.TextField()                # 글자 수 제한이 없는 데이터
+    create_date = models.DataTimeField()        # 날짜, 시간 관련 속성
+
