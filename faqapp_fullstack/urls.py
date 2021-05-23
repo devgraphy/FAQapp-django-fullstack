ad4c5770bbca5195b87012c1ec089090a9985cf7
Changjun Kim <beaican530@gmail.com>
2021-05-23 15:36:55 +0900

    Update url mapping:urls->faqapp.urls

--- faqapp_fullstack/urls.py
+++ faqapp_fullstack/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from faqapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('faq/',views.index),
+    path('faq/',include('faqapp.urls')),
 ]
